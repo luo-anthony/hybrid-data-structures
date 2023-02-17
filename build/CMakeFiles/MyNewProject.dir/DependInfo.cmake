7515f61a6cb817b57c190c8321ae171bbe450509
luo-anthony <anthony.luo@columbia.edu>
2023-02-17 13:28:07 -0500

    template building

---
+++ build/CMakeFiles/MyNewProject.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/anthonyluo/Documents/hybrid-data-structures/src/tmp.cpp" "CMakeFiles/MyNewProject.dir/src/tmp.cpp.o" "gcc" "CMakeFiles/MyNewProject.dir/src/tmp.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyNewProject.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "lib/Debug/libMyNewProject.a"
+)
